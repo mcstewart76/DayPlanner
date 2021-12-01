87f10d2c46e7bb3916f2e21222a4662ff84a9bbd
mcstewart76 <mcstewart76@gmail.com>
2021-12-01 14:06:10 -0500

    Adding time functionality to javascript file

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+//Moment times
+var currentTime = moment();
+// append times to blocks
+var currentHour = currentTime.format("hA")
+
+if(currentTime.format("hA") == currentHour){
+ $("#hour").text(currentHour);   
+ $("#descriptBox").addClass("present");
+//  $("#descriptBox").removeClass("past")
+
+}
+
+//create rows by appending classes
+
+
+//text box
+//at current time change color
+//using if statement and local variable for current time
+
+//save button on click function
+//save button local storage
