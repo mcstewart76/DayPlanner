90cf99f790790e79649bc51ce5863999ba56483d
mcstewart76 <mcstewart76@gmail.com>
2021-12-01 16:59:13 -0500

    Added an array with variables to cycle through to append rows and classes to

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,34 @@
 //Moment times
+const rowAdd = document.getElementsByClassName("rowAdder");
 var currentTime = moment();
 // append times to blocks
-var currentHour = currentTime.format("hA")
 
-if(currentTime.format("hA") == currentHour){
- $("#hour").text(currentHour);   
+var timeBlock0 = moment().subtract(3,'h').format('hA');
+var timeBlock1 = moment().subtract(2,'h').format('hA');
+var timeBlock2 = moment().subtract(1,'h').format('hA');
+var timeBlock3 = currentTime.format("hA")
+var timeBlock4 = moment().add(1,'h').format('hA');
+var timeBlock5 = moment().add(2,'h').format('hA');
+var timeBlock6 = moment().add(3,'h').format('hA');
+var timeBlock7 = moment().add(4,'h').format('hA');
+var timeBlock8 = moment().add(5,'h').format('hA');
+const timeBlocks = [timeBlock0, timeBlock1, timeBlock2, timeBlock3, timeBlock4, timeBlock5, timeBlock6, timeBlock7, timeBlock8];
+$.each(timeBlocks, function(name, value){
+console.log(`${name} = ${value}`)
+
+});
+if(currentTime.format("hA") == timeBlock3){
+ $("#hour").text(timeBlock3);   
  $("#descriptBox").addClass("present");
 //  $("#descriptBox").removeClass("past")
-
+}
+if(currentTime.format("hA") < timeBlock3){
+       
+    $("#descriptBox").addClass("future");
+}
+if(currentTime.format("hA") > timeBlock3){
+       
+    $("#descriptBox").addClass("past");
 }
 
 //create rows by appending classes
