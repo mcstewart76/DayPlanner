002fc431c2191d3b7339a52c1bab1556dd6bde2d
mcstewart76 <mcstewart76@gmail.com>
2021-12-02 23:50:17 -0500

    Added functionality to button, added local storage functionality, adjusted javascript with jQuery to simplify code

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,13 +16,16 @@ var timeBlock5 = moment().add(2,'h').format('hA');
 var timeBlock6 = moment().add(3,'h').format('hA');
 var timeBlock7 = moment().add(4,'h').format('hA');
 var timeBlock8 = moment().add(5,'h').format('hA');
+var timeBlock9 = moment().add(6,'h').format('hA');
 curTime.innerHTML = currentDate; 
-const timeBlocks = [timeBlock0, timeBlock1, timeBlock2, timeBlock3, timeBlock4, timeBlock5, timeBlock6, timeBlock7, timeBlock8];
-$('.saveBtn').on('click', function(){
-    // var descriptionBox = 
-    // var currentDescriptTime = 
-    // localStorage.setItem(descriptionBox, currentDescriptTime);
-});
+const timeBlocks = [timeBlock0, timeBlock1, timeBlock2, timeBlock3, timeBlock4, timeBlock5, timeBlock6, timeBlock7, timeBlock8, timeBlock9];
+
+// $(".saveBtn").on("click", "button", function(){
+//     console.log("hello")
+// })
+
+
+
 
 $.each(timeBlocks, function(name, value){
     //timeBlocks[index].classList.add(name)
@@ -38,33 +41,60 @@ $.each(timeBlocks, function(name, value){
    rowDiv.appendChild(rowHour);
     
     const rowDescript = document.createElement('textarea');
-    rowDescript.classList.add('description', 'px-1', 'd-flex', 'descriptBox', 'time-block');
-    rowDescript.setAttribute("id", value);
+    rowDescript.classList.add('description', 'px-1', 'd-flex', 'descriptBox', 'time-block', value);
+    // rowDescript.classList.add("class", value);
     rowDiv.appendChild(rowDescript);
     
     const rowSave = document.createElement('button');
-    rowSave.classList.add('saveBtn', 'm-0', 'd-flex');
+    rowSave.classList.add('saveBtn','m-0', 'd-flex', 'justify-content-center', 'align-items-center');
+    rowSave.innerHTML = "💾";
     rowSave.setAttribute("id", value);
     rowDiv.appendChild(rowSave);
 
     rowAdd.appendChild(rowDiv)
 
+    $(".description.".concat(value)).val(localStorage.getItem(value))
+
 });
 
 $.each($('.hour'), function(index, value) {
-    if(index > 4){
-        document.querySelector("#rowAdder > div:nth-child(5) > textarea").classList.add('future')
-        document.querySelector("#rowAdder > div:nth-child(6) > textarea").classList.add('future')
-        document.querySelector("#rowAdder > div:nth-child(7) > textarea").classList.add('future')
-        document.querySelector("#rowAdder > div:nth-child(8) > textarea").classList.add('future')
-        document.querySelector("#rowAdder > div:nth-child(9) > textarea").classList.add('future')
+    if(index > 3){
+        // document.querySelector("#rowAdder > div:nth-child(5) > textarea").classList.add('future')
+        // document.querySelector("#rowAdder > div:nth-child(6) > textarea").classList.add('future')
+        // document.querySelector("#rowAdder > div:nth-child(7) > textarea").classList.add('future')
+        // document.querySelector("#rowAdder > div:nth-child(8) > textarea").classList.add('future')
+        // document.querySelector("#rowAdder > div:nth-child(9) > textarea").classList.add('future')
+       $(".description.".concat(value.innerHTML)).addClass('future')
+       console.log(".description.".concat(value.innerHTML))
+        console.log(value.innerHTML)
     }
-    else if(index = 4){
-        document.querySelector("#rowAdder > div:nth-child(4) > textarea").classList.add('present')
+     if(index < 3){
+       // document.querySelector("#rowAdder > div:nth-child(4) > textarea").classList.add('present')
+        $(".description.".concat(value.innerHTML)).addClass('past')
+    }else{
+     //$(".descriptBox").addClass("past") 
+     $(".description.".concat(value.innerHTML)).addClass('present')  
     }
-    $(".descriptBox").addClass("past")
     
+    
+
+
 });
+
+
+
+$(".saveBtn").on("click",  function(){
+    var  Time = $(this).attr('id');
+    //var tA = 'textarea#'.concat(Time);
+    var descriptBox = $(".description.".concat(Time)).val();
+    console.log( $(".description.".concat(Time)).val())
+    console.log($(this));
+    //console.log(tA);
+    //console.log(descriptBox);
+   // console.log("hwl")
+    localStorage.setItem(Time, descriptBox)
+})
+
 
 
 //create rows by appending classes
