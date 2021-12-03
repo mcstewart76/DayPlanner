3da5c10c8f28de905fd2891b7383db647940306d
mcstewart76 <mcstewart76@gmail.com>
2021-12-02 19:54:34 -0500

    Added ids to each box, also updated the readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# Day Planner
+Provided an original html code and style sheet, the goal was to create a daily planner that highlights current hour, showing past in grey, and future in green. Gives user a way to save their schedule in local storage for future usage.
+
+## Day Planner 
+
+[Check it out here!](https://mcstewart76.github.io/DayPlanner/)
+
+>**Picture of the site**
+
+![This webpage includes a dynamic time sheet for all your daily scheduling needs](./assets/images/DayPlan.JPG)
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,11 @@
 const rowAdd = document.getElementById("rowAdder");
 const timeGrab = document.getElementsByClassName('hour');
 const curTime = document.getElementById('currentDay')
+
 var currentTime = moment();
 // append times to blocks
+
 var currentDate = moment().format('MMMM Do, YYYY');
-curTime.innerHTML = currentDate ;
 var timeBlock0 = moment().subtract(3,'h').format('hA');
 var timeBlock1 = moment().subtract(2,'h').format('hA');
 var timeBlock2 = moment().subtract(1,'h').format('hA');
@@ -15,29 +16,38 @@ var timeBlock5 = moment().add(2,'h').format('hA');
 var timeBlock6 = moment().add(3,'h').format('hA');
 var timeBlock7 = moment().add(4,'h').format('hA');
 var timeBlock8 = moment().add(5,'h').format('hA');
-
+curTime.innerHTML = currentDate; 
 const timeBlocks = [timeBlock0, timeBlock1, timeBlock2, timeBlock3, timeBlock4, timeBlock5, timeBlock6, timeBlock7, timeBlock8];
-$.each(timeBlocks, function(name, value){
+$('.saveBtn').on('click', function(){
+    // var descriptionBox = 
+    // var currentDescriptTime = 
+    // localStorage.setItem(descriptionBox, currentDescriptTime);
+});
 
+$.each(timeBlocks, function(name, value){
+    //timeBlocks[index].classList.add(name)
+    
+    
     const rowDiv = document.createElement('div');
     rowDiv.classList.add('row', 'justify-content-center', 'd-flex', 'flex-nowrap');
-
     const rowHour = document.createElement('div');
     rowHour.classList.add('hour', 'pl-3', 'd-flex');
+    rowHour.setAttribute("id", value);
     rowHour.innerHTML = value;
 
    rowDiv.appendChild(rowHour);
     
     const rowDescript = document.createElement('textarea');
     rowDescript.classList.add('description', 'px-1', 'd-flex', 'descriptBox', 'time-block');
+    rowDescript.setAttribute("id", value);
     rowDiv.appendChild(rowDescript);
     
     const rowSave = document.createElement('button');
     rowSave.classList.add('saveBtn', 'm-0', 'd-flex');
+    rowSave.setAttribute("id", value);
     rowDiv.appendChild(rowSave);
 
     rowAdd.appendChild(rowDiv)
-
 
 });
 
@@ -48,13 +58,13 @@ $.each($('.hour'), function(index, value) {
         document.querySelector("#rowAdder > div:nth-child(7) > textarea").classList.add('future')
         document.querySelector("#rowAdder > div:nth-child(8) > textarea").classList.add('future')
         document.querySelector("#rowAdder > div:nth-child(9) > textarea").classList.add('future')
-    }else if(index = 4){
+    }
+    else if(index = 4){
         document.querySelector("#rowAdder > div:nth-child(4) > textarea").classList.add('present')
     }
     $(".descriptBox").addClass("past")
     
-    
-  });
+});
 
 
 //create rows by appending classes
